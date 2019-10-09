f826e54642d22cb15346c66939291994b58e58cb
sviatpov <sviat.povod@gmail.com>
2019-10-09 21:08:28 +0300

    refactor model

--- Project_3/model.py
+++ Project_3/model.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 class Iter():
     def __init__(self):
         self.iter = 0
+
     @property
     def get(self):
         self.iter = self.iter+1
@@ -12,58 +13,49 @@ class Iter():
 class LeNet():
     def __init__(self, in_shape=[None, 32, 32, 3]):
         self.init_shape = in_shape
-        self.count = Iter()
-
-    @staticmethod
-    def _estimate_out_shape(n, f, p, s):
-        '''
-        Calculate output size of image after conv layer
-
-        :param n:image size
-        :param f:filter size
-        :param p:padding
-        :param s:strides
-        :return:output size
-        '''
-        return int((n + 2.0 * p - f)/s) +1
 
     def build_model(self):
         input = tf.placeholder(shape=self.init_shape, dtype=tf.float32)
 
-        # 32x32x3
-        conv_1 = self.convolution2D(1, input, 6, 5, 1, 'VALID')
-        print("Conv_1_Layer: ", conv_1.get_shape())
-        # 28x28x6
+        print("Input: ", input.get_shape())
 
-        maxp_1c= tf.nn.max_pool(conv_1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-        print("MaxPull_1: ", maxp_1c.get_shape())
+        with tf.variable_scope('Block_1'):
+            conv_1 = self.convolution2D(1, input, 6, 5, 1, 'VALID')
+            print("Conv Block 1: ", conv_1.get_shape(), " Weights: ", self.trainable_variable())
+            maxp_1c= tf.nn.max_pool(conv_1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+            print("MaxPulling 1: ", maxp_1c.get_shape(), " Weights: ", self.trainable_variable())
 
 
         # 14x14x6
+        with tf.variable_scope('Block_2'):
+            conv_2 = self.convolution2D(2, maxp_1c, 16, 5, 1, padding='VALID')
+            print("Conv Block 2: ", conv_2.get_shape(), " Weights: ", self.trainable_variable())
+            maxp_2 = tf.nn.max_pool(conv_2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+            print("MaxPulling 2: ", maxp_2.get_shape(), " Weights: ", self.trainable_variable())
 
-        conv_2 = self.convolution2D(2, maxp_1c, 16, 5, 1, padding='VALID')
+        with tf.variable_scope('Flatten'):
+            fl = tf.layers.flatten(maxp_2)
+            print("Flatten: ", fl.get_shape(), " Weights: ", self.trainable_variable())
 
-        print("Conv_2_Layer: ", conv_2.get_shape())
+        with tf.variable_scope('Block_3'):
+            fc_1 = self.fully_conected(3, fl, 120)
+            print("Fully Block 1: ", fc_1.get_shape(), " Weights: ", self.trainable_variable())
 
-        maxp_2 = tf.nn.max_pool(conv_2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-
-        print("max_2_Layer: ", maxp_2.get_shape())
-
-        fl = tf.layers.flatten(maxp_2)
-        print("After flatten: ", fl.get_shape())
-
-        fc_1 = self.fully_conected(3, fl, 120)
-
-        fc_2 = self.fully_conected(4, fc_1, 84)
+        with tf.variable_scope('Block_4'):
+            fc_2 = self.fully_conected(4, fc_1, 84)
+            print("Fully Block 2: ", fc_2.get_shape(), " Weights: ", self.trainable_variable())
 
         # TODO: Do i need relu here
-        fc_3 = self.fully_conected(4, fc_2, 10)
+        with tf.variable_scope('Block_5'):
+            fc_3 = self.fully_conected(5, fc_2, 10)
+            print("Fully Block 3: ", fc_3.get_shape(), " Weights: ", self.trainable_variable())
 
-        return fc_2
+        return fc_3
 
     def convolution2D(self, numOfLayer, inputIm, filters, size, stride, padding='SAME', is_batch_norm=True):
         """
-
+        Instead i should use tf.keras.layers
+        but its just for clarification
         :param numOfLayer: number of convolutional layer
         :param inputIm: input tensor  [batches, height, width, channels]
         :param filters: number of filters
@@ -75,12 +67,11 @@ class LeNet():
         channels = inputIm.get_shape()[3]
 
         with tf.variable_scope(str(numOfLayer) + "_convlayer"):
-            with tf.variable_scope(str(numOfLayer) + "_conv"):
-                filtervalues = tf.Variable(tf.truncated_normal([size, size, int(channels), filters], stddev=0.001))
-                biasis = tf.Variable(tf.constant(0.0, shape=[filters]))
-                conv = tf.nn.conv2d(inputIm, filtervalues, strides=[1, stride, stride, 1], padding=padding,
-                                    name=str(numOfLayer) + "_conv")
-                conv = tf.add(conv, biasis)
+            filtervalues = tf.Variable(tf.truncated_normal([size, size, int(channels), filters], stddev=0.001))
+            biasis = tf.Variable(tf.constant(0.0, shape=[filters]))
+            conv = tf.nn.conv2d(inputIm, filtervalues, strides=[1, stride, stride, 1], padding=padding,
+                                name=str(numOfLayer) + "_conv")
+            conv = tf.add(conv, biasis)
 
 
 
@@ -93,19 +84,21 @@ class LeNet():
                     relu = tf.layers.batch_normalization(inputs=conv, axis=-1)
             return relu
 
-    def fully_conected(self, numOfLayer, input, out_size):
+    def fully_conected(self, numOfLayer, input, out_size, activation=True):
         # TODO: add scopes
         # TODO: Add batch norm
         # assert len(input.get_shape()) == 1
-        print("Len: ", input.get_shape()[1])
-        insize = input.get_shape()[1]
-        fc_w = tf.Variable(tf.truncated_normal([int(insize), out_size], stddev=0.001))
-        fc_b = tf.Variable(tf.zeros(out_size))
-        ret = tf.matmul(input, fc_w) + fc_b
-        return tf.nn.relu(ret, name=str(numOfLayer)+"_relu")
+        with tf.variable_scope(str(numOfLayer) + "_fully"):
+            insize = input.get_shape()[1]
+            fc_w = tf.Variable(tf.truncated_normal([int(insize), out_size], stddev=0.001))
+            fc_b = tf.Variable(tf.zeros(out_size))
+            ret = tf.matmul(input, fc_w) + fc_b
+            return ret if activation == False else tf.nn.relu(ret)
 
     def batch_normal(self,numOfLayer, input):
-        '''
+        '''        # I did this function just to understand am I right about my batch norm intuition
+
+
         I want to compare it with tf.layers.batch_normalization
         :param numOfLayer:
         :param input:
@@ -114,7 +107,7 @@ class LeNet():
         size = input.shape[0]
         mean, var = tf.nn.moments(input, [0])
         beta = tf.Variable(tf.zeros(size))
-        scale = tf.Variable(tf.zeros(size))
+        scale = tf.Variable(tf.ones(size))
         with tf.variable_scope(str(numOfLayer) + "_batch"):
             ret = tf.nn.batch_normalization(input, mean, var, beta, scale, 0.001)
         return ret
